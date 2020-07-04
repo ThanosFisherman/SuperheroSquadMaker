cace956abb7c800e20753b6b3cc8df24876c4416
Thanos Psaridis <psaridis@gmail.com>
2020-07-04 10:38:24 +0300

    fancy little splash screen

--- presentation/src/main/java/com/thanosfisherman/presentation/MainActivity.kt
+++ presentation/src/main/java/com/thanosfisherman/presentation/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.thanosfisherman.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_SquadMaker)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
